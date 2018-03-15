ef1c323e06b207c242f233e13837eab25b42fb8a
ValyaAvetisyan <valyaavetisyan@gmail.com>
2018-03-15 22:05:25 +0400

    message is added

--- src/main/java/com/example/educenter/model/User.java
+++ src/main/java/com/example/educenter/model/User.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
@@ -24,6 +25,7 @@ public class User {
     @Column
     private String surname;
     @Column
+    @NotNull(message = "email cannot be null")
     private String email;
     @Column
     private String password;
--- src/main/java/com/example/educenter/util/EmailServiceImpl.java
+++ src/main/java/com/example/educenter/util/EmailServiceImpl.java
@@ -7,13 +7,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class EmailServiceImpl {
-  
     @Autowired
     public JavaMailSender emailSender;
  
     public void sendSimpleMessage(
       String to, String subject, String text) {
-
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to); 
         message.setSubject(subject); 
